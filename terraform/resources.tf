c73c0b1b56077bf58a17ba527f57e12dafcdeb38
phananh.tuan <phananh.tuan@rakuten.com>
2021-10-21 18:12:06 +0900

    test

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -173,9 +173,9 @@ resource "null_resource" "remote_exec_from_github" {
       "sudo curl -L https://github.com/docker/compose/releases/download/1.29.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "sudo usermod -a -G docker ec2-user",
-
       "git clone https://github.com/tuan-phan/DevOps_Pratice.git",
       "cd DevOps_Pratice",
+      "docker-compose -v"
       "docker-compose build",
       "docker-compose up -d --force-recreate",
     ]
