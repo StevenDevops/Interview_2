8f8b6fb02b7c93364c4d78a73a55edf40f1dc258
phananh.tuan <phananh.tuan@rakuten.com>
2021-10-21 17:36:21 +0900

    test

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -165,10 +165,10 @@ resource "null_resource" "remote_exec_from_github" {
     private_key = var.AWS_SECRET_PRIVATE_KEY
   }
 
-#  provisioner "file" {
-#    source = "/github/workspace"
-#    destination = "/home/ec2-user/"
-#  }
+  provisioner "file" {
+    source = "/github/workspace/"
+    destination = "/home/ec2-user/"
+  }
 
   provisioner "remote-exec" {
     inline = [
@@ -178,6 +178,7 @@ resource "null_resource" "remote_exec_from_github" {
       "sudo curl -L https://github.com/docker/compose/releases/download/1.29.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
       "sudo usermod -a -G docker ec2-user",
+      "ls /home/ec2-user/"
       "cd /home/ec2-user/workspace",
       "docker-compose build",
       "docker-compose up -d --force-recreate",
